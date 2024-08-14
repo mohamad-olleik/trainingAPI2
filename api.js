ca708337b29e63b7e57417dc3878ce29856f3d61
mohamad olleik <mhmd.olleik17@gmail.com>
2024-08-14 13:36:30 +0300

    c5

--- api.js
+++ api.js
@@ -2,6 +2,16 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3030;
 
+const xlsx=require('xlsx')
+let wb=xlsx.readFile('data.xlsx');
+let ws=wb.Sheets['Sheet1'];
+let data=xlsx.utils.sheet_to_json(ws);
+data.forEach(element => {
+  console.log(element.id)
+  
+});
+
+
 
 // your code
 
@@ -29,6 +39,14 @@ app.get('/', function (req, res) {
 
   app.get('/test3', function (req, res) {
     res.send('great job')
+  })
+
+  app.get('/data', function (req, res) {
+    let students=[];
+    data.forEach(element=>{
+      students.push(element.name)
+    })
+    res.send(students)
   })
 
 
